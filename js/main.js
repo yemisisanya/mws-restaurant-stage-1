5b1a063256c8f52c0449a3222405560856ef535d
YemisiSanya <yemisi06@gmail.com>
2018-04-03 19:27:42 -0600

    Secon commit

--- js/main.js
+++ js/main.js
@@ -141,6 +141,7 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
+  image.alt = "Image of" + restaurant.name + " Restaurant";
   li.append(image);
 
   const name = document.createElement('h2');
--- js/restaurant_info.js
+++ js/restaurant_info.js
@@ -58,7 +58,7 @@ fillRestaurantHTML = (restaurant = self.restaurant) => {
   const image = document.getElementById('restaurant-img');
   image.className = 'restaurant-img'
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-
+  image.alt = "Image of" + restaurant.name + " Restaurant";
   const cuisine = document.getElementById('restaurant-cuisine');
   cuisine.innerHTML = restaurant.cuisine_type;
 
